a399b62a19d31da99ebf7ca2cc9f2a99b53413e0
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2023-12-20 21:13:55 +0700

    dss

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,5 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'cities';
+    protected $primaryKey = 'city_id';
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+
+    // biar tidak auto increment
+    public function getIncrementing(){
+        return false;
+    }
+
+    // mendevinisikan sebagai string
+    public function getKeyType(){
+        return 'string';
+    }
 }
