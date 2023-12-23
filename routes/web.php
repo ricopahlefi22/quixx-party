5d1a740065707c05eff6ac81126c041de9e4a539
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2023-12-23 19:55:16 +0700

    upodate

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ComponentController;
 
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginProcess');
+    Route::get('forgot-password', 'forgotPassword');
+    Route::post('forgot-password', 'forgotPasswordProcess');
+    Route::get('reset-password', 'resetPassword');
+    Route::post('reset-password', 'resetPasswordProcess');
+    Route::post('otp', 'otp');
+    Route::get('logout', 'logout');
+
+
+});
+
+
+// START CONTROLLER MASTER
 Route::group(['domain' => 'master.localhost'], function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::get('login', 'login')->name('login');
-        Route::post('login', 'loginProcess');
-        Route::get('forgot-password', 'forgotPassword');
-        Route::post('forgot-password', 'forgotPasswordProcess');
-        Route::get('reset-password', 'resetPassword');
-        Route::post('reset-password', 'resetPasswordProcess');
-        Route::post('otp', 'otp');
-        Route::get('logout', 'logout');
 
-
-    });
     include 'extras/master/BerandaRoute.php';
     include 'extras/master/MasterDataRoute.php';
     include 'extras/master/PerhitunganCepatRoute.php';
 
+});
+// END CONTROLLER MASTER-------------------------------------
 
 
+
+// START CONTROLLER ADMIN
+Route::controller(AdminController::class)->group(function () {
+    Route::get('beranda', 'beranda');
 
 });
+
+
 
 // Component example
 Route::prefix('component')->group(function () {
@@ -34,6 +48,6 @@ Route::prefix('component')->group(function () {
 });
 
 Route::get('/', function () {
- return view("admin.beranda");
+   return view("admin.beranda");
 });
 
